eebacca19359a548db56c52449a1a6a809d9abea
Ankit Verma <ankitverma_0810@yahoo.co.in>
2021-09-01 16:05:08 +0530

    Initial commit

--- src/events/expiration-complete-event.ts
+++
@@ -1,9 +0,0 @@
-import { Subjects } from './subjects';
-
-// This would be the information that will be passed to the NATS server
-export interface ExpirationCompleteEvent {
-	subject: Subjects.ExpirationComplete;
-	data: {
-		orderId: string;
-	}
-}
